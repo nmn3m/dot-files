8aef31f22ba8601043b003883e6368063debf500
Nour <nurmn3m@gmail.com>
2025-04-05 23:14:59 +0200

    updating nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,8 @@ require("lazy").setup({
 	require("plugins.indent-blankline"),
 	require("plugins.misc"),
 	require("plugins.comment"),
+	require("plugins.avante"),
+	-- require("plugins.gitblame"),
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
